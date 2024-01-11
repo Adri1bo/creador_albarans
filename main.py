1500a3657f9d433a5a4396ea0d850c278ca28ce8
Adrià Bové Salat <above@altcamp.local>
2024-01-11 13:12:06 +0100

    altre lector de qr

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ Created on Thu Jan 11 12:43:45 2024
 
 import qrcode
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
-import cv2
+from streamlit_qrcode_scanner import qrcode_scanner
 
 accio=st.radio('Què vols fer?',['Crear','Llegir'])
 
@@ -25,39 +24,8 @@ if accio == 'Crear':
     st.image('qr_code.png', caption='Codi QR', use_column_width=True)
 
 else:
-    class QRCodeReader(VideoTransformerBase):
-        def transform(self, frame):
-            # Convertim el frame a escala de grisos
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
-            # Creem un objecte detectar de codis QR
-            qr_code_detector = cv2.QRCodeDetector()
+    qr_code = qrcode_scanner(key='qrcode_scanner')
     
-            # Detectem codis QR a la imatge
-            decoded_objects, points, qr_code = qr_code_detector.detectAndDecodeMulti(gray)
-    
-            # Dibuixem quadrats al voltant dels codis QR detectats
-            if points is not None:
-                for i in range(len(points)):
-                    rect_points = points[i].astype(int)
-                    frame = cv2.polylines(frame, [rect_points], isClosed=True, color=(0, 255, 0), thickness=2)
-    
-                    # Mostrem el contingut del codi QR
-                    st.write("Codi QR detectat:", decoded_objects[i])
-    
-            return frame
-    
-    def main():
-        st.title("Lector de Codi QR amb Streamlit")
-    
-        webrtc_ctx = webrtc_streamer(
-            key="example",
-            video_transformer_factory=QRCodeReader,
-            async_transform=True,
-        )
-    
-        if webrtc_ctx.video_transformer:
-            st.video(webrtc_ctx)
-    
-    if __name__ == "__main__":
-        main()
+    if qr_code:
+        st.write(qr_code)
